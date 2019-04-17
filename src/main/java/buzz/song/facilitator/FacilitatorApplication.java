2805536ea5661ee28588a82c89eb15b484fa9ce1
kerem <kerem@signifai.io>
2019-04-16 18:45:36 -0700

    Spring boot skeleton

---
+++ src/main/java/buzz/song/facilitator/FacilitatorApplication.java
@@ -0,0 +1,13 @@
+package buzz.song.facilitator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilitatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilitatorApplication.class, args);
+	}
+
+}
