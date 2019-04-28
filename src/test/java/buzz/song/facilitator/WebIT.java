af21c92eb779d6e510524394957df296c4f6ed29
kerem <kerem@signifai.io>
2019-04-28 10:39:56 -0700

    let IT parse the JSON response so we can chain future ITs

--- src/test/java/buzz/song/facilitator/WebIT.java
+++ src/test/java/buzz/song/facilitator/WebIT.java
@@ -1,15 +1,19 @@
 package buzz.song.facilitator;
 
+import buzz.song.facilitator.model.Venue;
+import buzz.song.facilitator.model.request.VenueCreateRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -24,18 +28,28 @@ public class WebIT {
 	@Autowired
 	private MockMvc mockMvc;
 
+	@Autowired
+	private ObjectMapper objectMapper;
+
 	@Test
-	public void venueCreate() throws Exception {
-		final String venueCreateRequest = "{\"latitude\": 200,\"longitude\": 300}";
+	public void controllerTests() throws Exception {
+		final VenueCreateRequest createRequest = new VenueCreateRequest("venue", 200, 300);
+		final String createPayload = objectMapper.writeValueAsString(createRequest);
 
 		final MvcResult result = mockMvc.perform(post("/venue")
 				.contentType(MediaType.APPLICATION_JSON)
-				.content(venueCreateRequest)
+				.content(createPayload)
 		).andReturn();
 
-		mockMvc.perform(asyncDispatch(result))
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.latitude").value(200))
-				.andExpect(jsonPath("$.longitude").value(300));
+		final ResultActions actions = mockMvc.perform(asyncDispatch(result))
+				.andExpect(status().isOk());
+
+		final Venue venue = objectMapper.readValue(actions.andReturn().getResponse().getContentAsString(), Venue.class);
+
+		final String venueId = venue.getId();
+
+		Assert.assertEquals("venue", venue.getName());
+		Assert.assertEquals(200, venue.getLatitude(), 0);
+		Assert.assertEquals(300, venue.getLongitude(), 0);
 	}
 }
